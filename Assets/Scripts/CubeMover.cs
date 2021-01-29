dfc917ce00c26d95421ff77496bd5b0a67f8f7c6
Prog84 <bvv198424@rambler.ru>
2021-01-29 11:06:00 +1000

    Initial commit

---
+++ Assets/Scripts/CubeMover.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CubeMover : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private Transform _finalMovementPoint;
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.TryGetComponent(out Transporter transporter))
+        {
+            Move(_finalMovementPoint);
+        }
+    }
+
+    private void Move(Transform finalMovementPoint)
+    {
+        transform.position = Vector3.Lerp(transform.position, finalMovementPoint.position, _speed * Time.deltaTime);
+    }
+}
