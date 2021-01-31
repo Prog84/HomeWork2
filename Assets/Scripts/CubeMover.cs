b3055696f9f8fb50fc42c4382dddc2973074e4e4
Prog84 <bvv198424@rambler.ru>
2021-01-31 10:40:34 +1000

    Change collison stay to collision enter and exit

--- Assets/Scripts/CubeMover.cs
+++ Assets/Scripts/CubeMover.cs
@@ -5,11 +5,29 @@ public class CubeMover : MonoBehaviour
     [SerializeField] private float _speed;
     [SerializeField] private Transform _finalMovementPoint;
 
-    private void OnCollisionStay(Collision collision)
+    private bool iSTransporter = false;
+
+    private void Update()
+    {
+        if (iSTransporter)
+        {
+            Move(_finalMovementPoint);
+        }        
+    }
+
+    private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.TryGetComponent(out Transporter transporter))
         {
-            Move(_finalMovementPoint);
+            iSTransporter = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.TryGetComponent(out Transporter transporter))
+        {
+            iSTransporter = false;
         }
     }
 
